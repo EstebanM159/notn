1426d8bb5a1da30796346fae78fff9853537760b
EstebanM159 <estebanm91231@gmail.com>
2024-04-17 11:38:35 -0300

    fix modal view

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -15,33 +15,36 @@ const Modal = () => {
               <Atropos rotateXMax={5} rotateYMax={5} shadow={false}>
                   {loading
                     ? (<Spinner></Spinner>)
-                    : (<div className='flex flex-row-reverse bg-neutral-900 justify-between rounded-md'>
-                    <div className='flex flex-col items-center w-full px-5'>
-                    <button
-                        className="bg-transparent border-0 text-white text-right w-full p-0 mt-3 mr-3 float-right"
-                        onClick={ closeModal }
-                    >x</button>
-                        <h3 className="text-white w-full pt-4 text-4xl font-bold my-3 text-start">
+                    : (<div className='flex flex-col-reverse md:flex-row-reverse mx-3 bg-neutral-900 justify-between rounded-md'>
+                    <div className='flex flex-col items-center md:w-full px-5'>
+                        <h3 className="text-white w-full pt-4 text-lg md:text-4xl font-bold my-3 text-start">
                             {name}
                         </h3>
-                            <span className='w-3/4 h-px bg-white'></span>
+                            <span className=' w-full md:w-3/4 h-px bg-white'></span>
 
                           <div className='flex gap-3 items-start w-full  h-auto flex-wrap py-4'>
                             {contentModal.genres.slice(0, 4).map(genre => (
-                              <p key={genre.id} className='text-cyan-300 bg-neutral-800 py-1 px-2 rounded-md'>{genre.name}</p>
+                              <p key={genre.id} className='text-cyan-300 text-xs md:text-lg bg-neutral-800 py-1 px-2 rounded-md'>{genre.name}</p>
                             ))}
                           </div>
 
-                        <p className=''>
+                        <p className='pb-5'>
                             {contentModal.overview}
                         </p>
 
+                      </div>
+
+                      <img className='w-full  md:hidden' src={`https://image.tmdb.org/t/p/w500/${contentModal.poster_path}`} alt={`Poster de ${contentModal.name}`} />
+                      <img className='w-60 hidden md:block md:w-full' src={`https://image.tmdb.org/t/p/w342/${contentModal.poster_path}`} alt={`Poster de ${contentModal.name}`} />
                     </div>
-                    <img src={`https://image.tmdb.org/t/p/w342/${contentModal.poster_path}`} alt={`Poster de ${contentModal.name}`} />
-                    </div>)
+
+                      )
                   }
                   </Atropos>
-
+                  <button
+                        className="bg-transparent border-0 text-white text-center w-full p-0 mt-3 mr-3 float-right"
+                        onClick={ closeModal }
+                    >x</button>
             </div>
           </div>
         </>
